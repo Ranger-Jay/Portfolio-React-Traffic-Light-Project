62df5cfdb7ac9b709dea46ee6aaa57d787929b99
Ranger-Jay <careerforjay@gmail.com>
2023-05-17 17:16:44 +0000

    completed with click, hook used

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [selectedColor, setSelectedColor] = useState("");
+  return (
+    <div className="center">
+      <div className="wire"></div>
+
+      <div className="box">
+        <br />
+        <div
+          onClick={() => setSelectedColor("red")}
+          className={"light red " + (selectedColor === "red" ? "ambient" : "")}
+        ></div>
+        <br />
+
+        <div
+          onClick={() => setSelectedColor("yellow")}
+          className={
+            "light yellow " + (selectedColor === "yellow" ? "ambient" : "")
+          }
+        ></div>
+        <br />
+
+        <div
+          onClick={() => setSelectedColor("green")}
+          className={
+            "light green " + (selectedColor === "green" ? "ambient" : "")
+          }
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
